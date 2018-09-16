628a9e28189fee8d5e31e50f3601330a98437200
sesang06 <sesang06@naver.com>
2018-09-16 19:36:36 +0900

    프로그램 정보, 메일 문의

--- iOS-txtreader/Settings/OptionsViewController.swift
+++ iOS-txtreader/Settings/OptionsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
-
-class OptionsViewController : UITableViewController {
+import UIKit
+import MessageUI
+class OptionsViewController : UITableViewController , MFMailComposeViewControllerDelegate {
     let options : [String] = ["저장 공간 관리" , "정보",  "설정" ,"내 계정", "도움말" , "제작자 정보", "문의하기", "프로그램 정보" ]
     let settings : [String] = ["자동 정렬", "빈칸 삽입", "워드랩", "텍스트 인코딩", "줄 간격", "글자" ,"배경 색" , "마지막 페이지 이어보기", "상태바 표시", "안티앨리어스 사용", "여백", "글꼴", "숨김 파일 표시", ""]
     let cellId = "cellId"
@@ -42,10 +43,51 @@ class OptionsViewController : UITableViewController {
                 self.revealViewController().revealToggle(self)
             }
             break
+        case 6:
+            sendMail()
+        case 7:
+            let vc = ProgramInfoViewController()
+            let uv = UINavigationController(rootViewController: vc)
+            self.present(uv, animated: true) {
+                self.revealViewController().revealToggle(self)
+            }
+            break
         default:
             break
         }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    func sendMail(){
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+            print("can send mail")
+        } else {
+       //     self.showSendMailErrorAlert()
+        }
+        
+    }
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        mailComposerVC.setToRecipients(["sesang06@naver.com"])
+        mailComposerVC.setSubject("버그 리포트")
+        mailComposerVC.setMessageBody("", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "메일을 전송 실패", message: "아이폰 이메일 설정을 확인하고 다시 시도해주세요.", delegate: self, cancelButtonTitle: "확인")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
     
 }
 
--- iOS-txtreader/Settings/ProgramInfoViewController.swift
+++ iOS-txtreader/Settings/ProgramInfoViewController.swift
@@ -8,4 +8,35 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
+class ProgramInfoViewController : UIViewController {
+    lazy var appVersionLabel : UILabel = {
+        let label = UILabel()
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            label.text = version
+        }
+        label.textAlignment = .center
+        return label
+    }()
+    override func viewDidLoad() {
+        setUpNavigaionBar()
+        setUpViews()
+    }
+    func setUpViews(){
+        view.backgroundColor = .white
+        view.addSubview(appVersionLabel)
+        appVersionLabel.snp.makeConstraints { (make) in
+            make.trailing.leading.top.bottom.equalTo(view)
+        }
+        
+    }
+    @objc func close(_ sender : Any){
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    func setUpNavigaionBar(){
+        self.navigationItem.title = "프로그램 정보"
+        let backButton = UIBarButtonItem(title: "닫기", style: UIBarButtonItemStyle.plain, target: self, action: #selector(close))
+        self.navigationItem.leftBarButtonItem = backButton
+    }
+}
