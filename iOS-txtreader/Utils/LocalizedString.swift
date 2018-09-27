c382c3b69070d262e5301cc28c8396e29424fc3f
sesang06 <sesang06@naver.com>
2018-09-28 01:14:13 +0900

    로컬 메시지..

---
+++ iOS-txtreader/Utils/LocalizedString.swift
@@ -0,0 +1,46 @@
+//
+//  LocalizedString.swift
+//  iOS-txtreader
+//
+//  Created by 조세상 on 28/09/2018.
+//  Copyright © 2018 조세상. All rights reserved.
+//  로컬라이즈드 된 스트링 목록들..
+
+struct LocalizedString {
+    let confirm = "confirm".localized //확인
+    let cancel = "cancel".localized //취소
+    let createBroswerMessage = "createBrowerMessage".localized // 폴더 생성
+    let local = "local".localized //로컬
+    let recent = "recent".localized //최근
+    let iCloud = "iCloud".localized //아이클라우드
+    let bookmark = "bookmark".localized //북마크
+    
+    let textFont = "textFont".localized //글씨체
+    let textSize = "textSize".localized //글씨 크기
+    let textColor = "textColor".localized
+    let textSample = "textSample".localized
+    let textViewMode = "textViewMode".localized
+    let setting = "setting"
+    let close = "close"
+    let textViewerSetting = "textViewerSetting"
+    let help = "help"
+    let inquiry = "inquiry"
+    let developerInfomation = "developerInfomation"
+    let emailSendFailMessage = "emailSendFailMessage"
+    let moveFile = "moveFile"
+    let error = "error"
+    let folderExistsErrorMessage = "folderExistsErrorMessage"
+    
+    let fileExistsErrorMessage = "fileExistsErrorMessage"
+    let deleteFileConfirmMessage = "deleteFileConfirmMessage"
+    let deleteRecentOpenedFile = "deleteRecentOpenedFile"
+    let deleteRecentOpenedFileMessage = "deleteRecentOpenedFileMessage"
+    
+    let noSearchFileMessage = "noSearchFileMessage"
+    let noFileMessage = "noFileMessage"
+    let loadingStart = "loadingStart"
+    let fileReadErrorMessage = "fileReadErrorMessage"
+    
+    let keywordSearchErrorMessage = "keywordSearchErrorMessage"
+    
+}
